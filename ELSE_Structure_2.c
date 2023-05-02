5adf85162dbc1ddfaab77782665b1ea6cf301862
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-02 22:01:35 +0700

    Create ELSE_Structure_2.c

---
+++ ELSE_Structure_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h> 
+
+int isPalindrome(char a[])
+{
+    int n= strlen(a);
+    for (int i =0; i<n;i++){
+        if (a[i]!=a[n-i-1]) {
+            return false;
+    }
+    }
+    return true;
+}
+
+int main(){
+    int len;
+    char a[500];
+    fgets(a,500,stdin);
+    len = strlen(a);
+if (a[len-1]=='\n') 
+{
+    len--;
+    a[len]='\0';
+}
+    puts(a);
+    // printf("\n");
+    // isPalindrome(a);
+    if (isPalindrome(a)) {
+        printf("YES");
+    }
+    else printf("NO");
+}
